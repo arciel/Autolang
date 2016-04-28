c746f3acc9ef3409d768c6479cef8b68e9c0455c
Tushar Rakheja <tusharrakheja@gmail.com>
2016-04-28 00:15:02 -0500

    Tentative methods to support the Set data type.

---
+++ Files_V2/Set.cpp
@@ -0,0 +1,101 @@
+#include "Set.h"
+
+/* Implementations for methods in the class Set. */
+
+Set::Set() : Elem(SET)                     // Default constructor.
+{
+	elems = new vector < Elem * >;
+}
+
+Set::Set(vector<Elem *> *elems): Elem(SET) // It is possible to initialize a set with an existing vector.
+{
+	this->elems = elems;                   // Just make the set's vector_pointer point to that existing vector.
+}
+
+int Set::cardinality()                     // Returns the cardinality of the set.
+{
+	return elems->size(); 
+} 
+
+Set Set::cartesian_product(Set &other)           // Returns the cartesian product of this set and the other set.
+{
+	Set cart;                                    // Make an empty set.
+	for (auto &elem_p1 : *elems)                 // For every element_pointer in the vector of element_pointers in this set ...
+		for (auto &elem_p2 : *(other.elems))     // ... taken with every element pointer in the vector of element_pointers in the other ...
+			cart.elems->push_back(                         // ... <convoluted_code> Push into the cartesian_product (cart) ...
+			new Tuple(                                     // ... a pointer to a Tuple object, that is ... 
+			new vector < Elem * >{ elem_p1, elem_p2 })     // ... constructed using a vector initialized with elem_p1 and elem_p2.                                                 
+			);                                             // </convoluted_code>
+}
+
+bool Set::has(Elem &elem)                  // Looks for an element in the set.
+{
+	for (auto &elem_p : *elems)            // For each (any) element_pointer in the vector of element_pointers ...
+		if (*elem_p == elem)               // ... if the object pointed to by that element_pointer is == the argument ...
+			return true;                   // ... then return true.
+	return false;                          // Else return false.
+}
+
+Set Set::intersection(Set &other)          // Intersection with a second set.
+{
+	Set intersect;                                       // Make an initially empty set since we don't know what we may have to add.
+	for (int i{ 0 }; i < elems->size(); i++)             // For every element_pointer in this set, check to see if ...
+		if (other.has(*(*elems)[i]))                     // ... the element pointed to by it is in the other one too. If it is ...
+			intersect.elems->push_back((*elems)[i]);     // ... then add a pointer to that element to their intersection.
+	return intersect;
+}
+
+const Elem & Set::operator[](int index) const    // R-value access.
+{
+	return *(*elems)[index];                     // Return a reference to an element pointed to by the element_pointer at index. 
+}
+
+Elem *& Set::operator[](int index)               // L-value access.
+{
+	return(*elems)[index];                       // Return an element_pointer at index.     
+}
+
+bool Set::operator==(Set &other)                 // Checks two sets for equality.
+{
+	if (cardinality() != other.cardinality())    // If the cardinalities are different ... 
+		return false;                            // ... then they are obviously not equal.
+	return this->subset_of(other);               // But if they are the same, then if either is a subset of the other, they indeed are equal.
+}
+
+Set Set::subset(int start, int end)              // Returns a subset of the set containing elements in [start, end).
+{
+	Set subset;                                  // Make an empty set.
+	for (int i{ start }; i < end; i++)           // Start at the starting index, go up till the ending index.
+		subset.elems->push_back((*elems)[i]);    // Add all the element_pointers between the limits to the subset.
+	return subset;                               // Then return the subset.
+}
+
+bool Set::subset_of(Set &candidate_superset)     // Checks if this set is a subset of the candidate_superset.
+{
+	for (auto &elem_p : *elems)                  // For every (any) element_pointer in this set's vector ...
+		if (!candidate_superset.has(*elem_p))    // ... if the candidate_superset does not have a pointer to the object it refers to ...
+			return false;                        // ... return false.
+	return true;                                 // Else return true.
+}
+
+string Set::to_string()                          // Returns a string representation of the set.
+{
+	string representation{ "{ " };
+	int i{ 0 };
+	for (auto &elem_p : *elems)
+	{
+		representation += elem_p->to_string();   // Recursive, awesome representations. ;)
+		if (i != elems->size() - 1)
+			representation += ", ";
+	}
+	return representation + " }";
+}
+
+Set Set::_union(Set &other)                // Union with a second set.
+{
+	Set unified(this->elems);
+	for (auto &elem_p : *(other.elems))    // For every other element_pointer in the other set's vector ...
+		if (!this->has(*elem_p))           // ... if the object pointed to by it is not already present in this set ...
+			elems->push_back(elem_p);      // ... add a pointer to it to this set as well.
+	return unified;
+}
---
+++ Files_V2/Set.h
@@ -0,0 +1,32 @@
+#ifndef SET_H
+#define SET_H
+
+#include "Tuple.h"
+#include <vector>
+
+using std::vector;
+using std::string;
+
+/* Definition of a Set in Autolang. */
+
+class Set : public Elem
+{
+public:
+	vector<Elem *> *elems;                     // A set is internally implemented as a vector of element_pointers.
+
+	Set();                                     // Default constructor.
+	Set(vector<Elem *> *);                     // It is possible to initialize a set with an existing vector (well, a pointer to it).
+	int cardinality();                         // Returns the cardinality of the set.	
+	Set cartesian_product(Set &);              // Returns the cartesian product of this set and the other set.
+	bool has(Elem &);                          // Looks for an element in the set.
+	Set intersection(Set &);                   // Intersection with a second set.
+	Set subset(int, int);                      // Returns a subset of the set containing elements in [start, end).
+	bool subset_of(Set &);                     // Checks if this set is a subset of the candidate_superset.
+	const Elem &operator[](int) const;         // R-value access.
+	Elem *&operator[](int);                    // L-value access.
+	bool operator==(Set &);                    // Checks two sets for equality.
+	string to_string();                        // Returns a string representation of the set.
+	Set _union(Set &);                         // Union with a second set.
+};
+
+#endif
