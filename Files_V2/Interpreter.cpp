bc0b125cad830d9ef38766d8b919254860fef713
Tushar Rakheja <tusharrakheja@gmail.com>
2016-05-03 22:03:13 -0500

    More includes.

--- Files_V2/Interpreter.cpp
+++ Files_V2/Interpreter.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include "Set.h"
+#include "Int.h"
+#include "Logical.h"
+#include "String.h"
+#include "Char.h"
 
-int main() {
+using std::cout;
+using std::endl;
 
+int main() 
+{
+	string x = "1234";
+	string y = x;
+	x[2] = '5';
+	cout << x << " " << y << endl;
+	return 0;
 }
